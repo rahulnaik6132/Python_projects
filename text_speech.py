f10549a7640ca75595ec9012f6c2158a88ebe913
Rahul R Naik <130396582+rahulnaik6132@users.noreply.github.com>
2025-02-08 15:31:43 +0530

    Add files via upload

---
+++ text_speech.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import os 
+
+# Default texts
+myText = "You can use your computer or Android tablet to chat with your friends     through Google Message…Important: Your Google Account is used to:"
+
+# Type of languaue
+language = "en"
+output = gTTS(text=myText, lang=language, slow=False)
+output.save("output.mp3") # Output File Name
+
+# For System Sound Configuration
+os.system("output.mp3")
